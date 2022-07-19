6285c91864d3f5d283b15bad1ff294ddb3238a7a
suychoi <96387244+suychoi@users.noreply.github.com>
2022-07-19 16:39:34 +0900

    Add files via upload
    connection successful , and no continuous write on cluster

--- Program.cs
+++ Program.cs
@@ -12,17 +12,19 @@ namespace ConsoleApp2
         {
             // ctrl + F5 : 실행
 
-			// 시작
+			// 연결 시작
 			ConnectionMultiplexer redisCluster = null;
 
 			try
 			{
-				string connectString = "172.30.5.186:6379,172.30.5.167:6379";  //172.30.5.167 , 172.30.5.186
+				string connectString = "172.30.5.186:3950,172.30.5.186:1521,172.30.5.186:6379,172.30.5.167:1521,172.30.5.167:3950,172.30.5.167:6379";  
+				//172.30.5.186:3950,172.30.5.186:1521,172.30.5.186:6379,172.30.5.167:1521,172.30.5.167:3950,172.30.5.167:6379
+
 				var options = ConfigurationOptions.Parse(connectString);
 					options.AllowAdmin = true;
 					options.ConfigCheckSeconds = 10;
 					options.SyncTimeout = 1000000;
-					//  options.Password = "password";
+					options.Password = "foobared";
 				redisCluster = ConnectionMultiplexer.Connect(options);
 			}
 			catch (Exception e)
@@ -30,36 +32,41 @@ namespace ConsoleApp2
 				Console.WriteLine(e);
 			}
 
-
-			IDatabase clusterDb = redisCluster.GetDatabase();
-
 			/*
 			string value1 = DateTime.Now.ToString("hh:mm:ss");
 
-			clusterDb.StringSet("mykey1", value1);
+			clusterDb.StringSet("test_connection", value1);
 
-			string result1 = clusterDb.StringGet("mykey1");
+			Console.WriteLine("write done");
+
+			string result1 = clusterDb.StringGet("test_connection");
 
 			Console.WriteLine(result1);
 			*/
 
+			
 			for(int i = 0; i < 101; i++)
             {
-				string key = "time" + i;				
-				string when = DateTime.Now.ToString("hh:mm:ss");
+				IDatabase clusterDb = redisCluster.GetDatabase();
 
-				clusterDb.StringSet(key, when);
+				string key = "time" + i;				
+				string t = DateTime.Now.ToString("hh:mm:ss");
+
+				Console.WriteLine("start write" + key + t);
+
+				clusterDb.StringSet(key, t);
+
+				Console.WriteLine("write done");
 
 				string result = clusterDb.StringGet(key);
 
 				Console.WriteLine(result);
 
-				Thread.Sleep(20000);   // 20초 대기
+				Thread.Sleep(200);   // 20초 대기
 
 			}
 		}
 	}
-
 }
 
 
